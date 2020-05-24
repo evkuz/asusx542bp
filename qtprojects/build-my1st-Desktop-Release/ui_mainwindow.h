e84ec0acb297a9ca622b9bb4ff5123a2febeea13
Evgeniy Kuznetsov <ekdubna@gmail.com>
2020-05-24 21:53:18 +0300

    24.05.2020 qt_projects generate_sample_mx

---
+++ qtprojects/build-my1st-Desktop-Release/ui_mainwindow.h
@@ -0,0 +1,81 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.5
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *my1st_button;
+    QTextEdit *textEdit;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(659, 377);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        my1st_button = new QPushButton(centralWidget);
+        my1st_button->setObjectName(QStringLiteral("my1st_button"));
+        my1st_button->setGeometry(QRect(50, 30, 171, 25));
+        textEdit = new QTextEdit(centralWidget);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+        textEdit->setGeometry(QRect(50, 100, 551, 111));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 659, 22));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        my1st_button->setText(QApplication::translate("MainWindow", "Welcome !!!", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
--- qtprojects/generate_sample_mx/main.cpp
+++ qtprojects/generate_sample_mx/main.cpp
@@ -1,8 +1,69 @@
 #include <QCoreApplication>
+#include <QtDebug>
+#include <QFile>
+#include <QFileInfo>
+#include <iostream>
+
+//#include <QRandomGenerator>
+
+#define accum_buffer_SIZE  0x400000 // это 4 194 304 байт !
+//#define mx_size 10000000 // Количество 4-байтовых элементов в матрице
+//#define arr_size 1000000
+//#define fpath  "/home/evkuz/asm/matrix_mul/data.bin"   //"./data.bin" // путь к создаваемому файлу на уровень вверх из папки debug/release
+
+#define A_fpath  "A_sample_data.bin"
+#define B_fpath  "B_sample_data.bin"
+
+#define A_size 20
+#define B_aize 15
+
+
+int gen_sample (QString fname, quint32 *databuf, quint32 buf_sz);
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    QString fname;
+    int numBytes;
+
+    quint32 mybuffer_A[]  = {11,13,15,17,19,12,14,16,18,20,31,33,35,37,39,42,44,46,48,50}; // mxA 4x5 arr_size
+    quint32 mybuffer_B[]  = {19,17,15,13,11,9,8,10,12,22,24,26,28,32,34};  // mxB 5x3
+
+    //char byInputBuffer[accum_buffer_SIZE]; //4096  0x40000// USB_Buffer_Size Buffer to hold data readed from
+
+
+    fname = A_fpath;
+    numBytes = gen_sample(fname, mybuffer_A, 20);
+    fname = B_fpath;
+    numBytes += gen_sample(fname, mybuffer_B, 15);
+return 0;
 }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+int gen_sample (QString fname, quint32 *databuf, quint32 buf_sz)
+{
+    QFile   mfile;
+    QString target_name;
+    quint32 j;
+    int ftdi_error_code = 0;
+
+    mfile.setFileName(fname);
+    if (!mfile.open(QIODevice::ReadWrite | QIODevice::Truncate )) { ftdi_error_code = 0x000D; } // Ошибка
+    else ftdi_error_code = 0x0013; // Нормально открылся
+
+    target_name = QFileInfo(QCoreApplication::applicationFilePath()).fileName();
+
+    for (j=0; j< buf_sz; j++)
+    {
+      ftdi_error_code = mfile.write(reinterpret_cast<const char*>(databuf + j), 4);
+    } //for j
+
+    qDebug() << target_name  << "/" << fname << " matrix file of " << buf_sz << " elements created !";
+    mfile.close(); // Тоже нужна проверка, что файл закрылся...
+   return ftdi_error_code;
+
+
+}
+
